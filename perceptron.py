bbbe786c307f1d8dd235900ca78fb0a851a5a336
sykoure <remi.arosio@gmail.com>
2018-11-27 16:01:22 +0100

    Propagation function is finished

--- perceptron.py
+++ perceptron.py
@@ -15,7 +15,7 @@ def aleaPoids():
 	tab = [[0 for k in range(5)] for k in range(4)]
 	for i in range(4):
 		for j in range(5):
-			tab[i][j] = random.uniform(0,1)
+			tab[i][j] = random.uniform(-1,1)
 	return tab
 			
 def propagation(poids,retine):
@@ -24,12 +24,17 @@ def propagation(poids,retine):
 		for j in range(5):
 			sortie = sortie + poids[i][j]*retine[i][j]
 	#Biais
-	sortie = sortie + random.uniform(0,1)
+	sortie = sortie + random.uniform(-1,1)
+	print(sortie)
+	if(sortie >= 0.5):
+		sortie = 1
+	else:
+		sortie = 0
 	return sortie
 	
 if __name__ == '__main__' :
 	c = copieInRetine('c')
 	a = copieInRetine('a')
 	poids = aleaPoids();
-
+	print(poids)
 	print(propagation(poids,a))
