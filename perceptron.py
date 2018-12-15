e345ba5a6cdd072262b2aca8660fa0915692885f
sykoure <remi.sykoure@hotmail.fr>
2018-12-15 03:07:00 +0100

    doesnt work anyway

--- perceptron.py
+++ perceptron.py
@@ -17,24 +17,50 @@ def aleaPoids():
 		for j in range(5):
 			tab[i][j] = random.uniform(-1,1)
 	return tab
-			
+
 def propagation(poids,retine):
 	sortie = 0
 	for i in range(4):
 		for j in range(5):
 			sortie = sortie + poids[i][j]*retine[i][j]
-	#Biais
+		#Biais
 	sortie = sortie + random.uniform(-1,1)
-	print(sortie)
-	if(sortie >= 0.5):
-		sortie = 1
-	else:
+	if(sortie <= 0):
 		sortie = 0
+	else:
+		sortie = 1
 	return sortie
-	
+
+def apprentissage(poids,retine,retine2,expectedOuputa,expectedOuputc):
+	sortie = 0
+	choix_retine = 1
+	sortie = propagation(poids,retine)
+	for i in range(10000):
+		if((sortie == 0)and(choix_retine == 1)):
+			for i in range(4):
+				for j in range(5):
+					poids[i][j] = poids[i][j] + 0.1 * (expectedOuputa - sortie) * retine[i][j]
+					choix_retine = choix_retine * -1
+		elif((sortie == 1)and(choix_retine == -1)):
+			for i in range(4):
+				for j in range(5):
+					poids[i][j] = poids[i][j] + 0.1 * (expectedOuputc - sortie) * retine2[i][j]
+					choix_retine = choix_retine * -1
+		else:
+			i = 10000
+	return poids
+
 if __name__ == '__main__' :
+
+	expectedValueA = 1.0
+	expectedValueC = 0.0
 	c = copieInRetine('c')
 	a = copieInRetine('a')
-	poids = aleaPoids();
+
+	poids = aleaPoids()
+	print(poids)
+	
+	apprentissage(poids,a,c,expectedValueA,expectedValueC)
+
 	print(poids)
 	print(propagation(poids,a))
